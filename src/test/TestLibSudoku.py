7cd9831e25478521456a1bc017784b74f9f3823a
Dominik Danter <dominik@foop.at>
2013-07-27 01:08:08 -0500

    created tests

---
+++ src/test/TestLibSudoku.py
@@ -0,0 +1,30 @@
+'''
+Created on 26/07/2013
+
+@author: dominik
+'''
+import unittest
+from lib.libSudoku import get_new_board, is_board_valid
+
+
+class Test(unittest.TestCase):
+
+    def testBoardCreationAndValidation(self):
+        for i in range(1,100):
+            newBoard = get_new_board()
+            self.assertTrue(is_board_valid(newBoard), "newly created board is not valid")
+
+        newBoard = get_new_board()
+        newBoard[0][0] = newBoard[2][2]
+        self.assertFalse(is_board_valid(newBoard), "invalid board deemed to be valid - group")
+        newBoard= get_new_board()
+        newBoard[0][8] = newBoard[0][0]
+        self.assertFalse(is_board_valid(newBoard), "invalid board deemd te be valid - row") 
+        newBoard= get_new_board()
+        newBoard[8][8] = newBoard[0][8]
+        self.assertFalse(is_board_valid(newBoard), "invalid board deemd te be valid - col") 
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
